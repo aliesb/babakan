592990cf147b78c5532d93b95e3c10171ea96714
aliesb <ali.esb91@gmail.com>
2025-05-19 00:32:03 +0330

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
+// firebase-config.js
 const firebaseConfig = {
     apiKey: "AIzaSyA08vHjfTM18hyZUjTfNtBpt-oC0xmgKFE",
     authDomain: "babakan-clinic.firebaseapp.com",
     projectId: "babakan-clinic",
     storageBucket: "babakan-clinic.appspot.com",
     messagingSenderId: "188319180301",
-        appId: "1:188319180301:web:383b8fc0dd0cdf06f95744"
+    appId: "1:188319180301:web:383b8fc0dd0cdf06f95744"
 };
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+export { firebaseConfig }; // export کردن firebaseConfig
